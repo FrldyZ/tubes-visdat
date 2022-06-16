5e2ca0fb2111b6f56d5f80d0ab34f505e7170eb8
Christhofer Laurent Juliant <94282400+FrldyZ@users.noreply.github.com>
2022-06-16 21:54:42 +0700

    Update visdattugasakhir.py

--- visdattugasakhir.py
+++ visdattugasakhir.py
@@ -229,6 +229,34 @@ select.js_on_change('value',
                     )
                   )
 
+fig1 =figure(title='Asian Paint Volume In Stock Market',
+           x_axis_label='Month-Year', y_axis_label='Price',
+           x_axis_type='datetime',
+           sizing_mode="stretch_width", plot_height=350,plot_width=750)
+fig1.line(x=df_asianpaint['Date'], y=df_asianpaint['Volume'], line_width=2, line_color="cyan")
+tab1 = Panel(child=fig1, title="Asian Paint")
+
+fig2 =figure(title='Tata Motors Volume In Stock Market',
+           x_axis_label='Month-Year', y_axis_label='Price',
+           x_axis_type='datetime',
+           sizing_mode="stretch_width", plot_height=350,plot_width=750)
+fig2.line(x=df_tatamotors['Date'], y=df_tatamotors['Volume'], line_width=2, line_color="red")
+tab2 = Panel(child=fig2, title="Tata Motors")
+
+fig3 =figure(title='Tata Motors Volume In Stock Market',
+           x_axis_label='Month-Year', y_axis_label='Price',
+           x_axis_type='datetime',
+           sizing_mode="stretch_width", plot_height=350,plot_width=750)
+fig3.line(x=df_reliance['Date'], y=df_reliance['Volume'], line_width=2, line_color="yellow")
+tab3 = Panel(child=fig3, title="Reliance")
+
+
+
+tabs = Tabs(tabs=[tab1, tab2, tab3])
+
+set_style(fig1)
+set_style(fig2)
+set_style(fig3)
 
 
 fig.add_tools(hover)
@@ -247,6 +275,7 @@ fig.legend.click_policy="hide"
 
 layout=layout([
                [select],
+               [tabs],
                [fig],
 ])
 
